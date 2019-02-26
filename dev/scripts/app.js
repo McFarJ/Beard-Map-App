c6ef8f3248ba67a01208f4b85472966e48b60bf2
Jason Siks <jsiks@pop-os.localdomain>
2019-02-26 15:31:40 -0500

    Final commit

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>My first app</h1>
+        <nav>
+          <ul><li>twitter</li><li>FB</li></ul>
+        </nav>
+      </header>
+    )
+  }
+}
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>twist a nip</p>
+    </footer>
+  )
+}
 class App extends React.Component {
   render() {
     return (
       <div>
-        Hello
+        <Header />
+        <Footer />
       </div>
     )
   }
