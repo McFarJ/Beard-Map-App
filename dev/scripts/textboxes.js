bdd2291fdf6792a93866716ae1c42d6a05fedf8a
Jason McFarlane <jasonpsmcfarlane@gmail.com>
2019-03-15 22:52:57 -0400

    connected Firebase, some styling

--- dev/scripts/textboxes.js
+++ dev/scripts/textboxes.js
@@ -1,51 +1,114 @@
 import React from 'react'
 import $ from 'jquery'
 
+
+export class instructionText extends React.Component{
+    render(){
+        return(
+            <div className="tips-text">
+                <p className="tips-text__t1">Select beard location</p>
+                <p className="tips-text__t2">Adjust the slider below</p>
+                <p className="tips-text__t3">Click save at the top of the page</p>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+
+
 export class AuthText extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            inputEmail: '', 
-            inputPassword: '', 
             email: "E-Mail", 
             password: "Password", 
+
+            createEmail: '',
+            createPassword: '',
+
             confirm: "Confirm Password"
         }
-
-        this.gargoyle = 'pink'
-        this.handleChange = this.handleChange.bind(this);
-        
-        // will probably need to unbind this..
-        this.getUserInfo = this.getUserInfo.bind(this);
+        this.handleChange = this.handleChange.bind(this)
+        this.createUser = this.createUser.bind(this)
+        this.signIn = this.signIn.bind(this)
+        this.handleSwitchForms = this.handleSwitchForms.bind(this)
     }
 
+    handleSwitchForms(e){
+        if(e.target.classList.contains('auth-text__form-select_active')){return}
+        else {
+            $('.auth-text__form-select_active').removeClass('auth-text__form-select_active')
+            $(e.target).addClass('auth-text__form-select_active')
+            $('.auth-text__form_active').removeClass('auth-text__form_active')
+            $('.auth-text__form_inactive').addClass('auth-text__form_active').removeClass('auth-text__form_inactive')
+            $('.auth-text__form').not('.auth-text__form_active').addClass('auth-text__form_inactive')
+        }
+    }
 
     handleChange(e){
-        var category = e.target.getAttribute('class');
-        var value = e.target.value; 
-        var newState = {};
-        newState[category] = value;
+        var category = e.target.getAttribute('class')
+        var value = e.target.value
+        var newState = {}
+        newState[category] = value
         this.setState(newState)
     }
 
-    getUserInfo(e){
-        e.preventDefault();
-        console.log('this.state.email: ' + this.state.email)
-        console.log('this.state.password: ' + this.state.password)
+    createUser(e){
+        e.preventDefault()
+        var email = this.state.email
+        var password = this.state.password
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch((error) => console.log(error.code, error.message))
     }
 
+    signIn(e){
+        e.preventDefault();
+        var email = this.state.email
+        var password = this.state.password
 
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((success) => {
+                console.log(`Logged in as ${success.email}`)
+                this.props.updateLogInStatus(true)
+            }), (error) => {
+                console.log(error);
+            }
+    }
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user) {
+                this.setState({loggedIn: true})
+                console.log('logged in')
+            } else {
+                this.setState({loggedIn: false})
+                console.log('not logged in')
+            }
+        })
+    }
 
     render(){
         return(
             <div className="auth-text auth-text_inactive">
-                <a>Log in</a>
-                <a>Create account</a>
-                <form>
+                <a className="auth-text__form-select auth-text__form-select_active" onClick={this.handleSwitchForms}>Create account</a>
+                <a className="auth-text__form-select" onClick={this.handleSwitchForms}>Log in</a>
+                <form className="auth-text__form auth-text__new-user-form auth-text__form_active" onSubmit={(e) => this.createUser(e)}>
+                    <input className="email" placeholder={this.state.email} onChange={this.handleChange}></input>
+                    <input className="password" placeholder={this.state.password} onChange={this.handleChange}></input>
+                    {/* FIX set value */}
+                    <input value={this.state.confirm}></input>
+                    <button>submit</button>
+                </form>
+                <form className="auth-text__form auth-text__log-in-form auth-text__form_inactive" onSubmit={(e) => this.signIn(e)}>
                     <input className="email" placeholder={this.state.email} onChange={this.handleChange}></input>
                     <input className="password" placeholder={this.state.password} onChange={this.handleChange}></input>
                     <input value={this.state.confirm}></input>
-                    <button onClick={this.getUserInfo}>submit</button>
+                    {/* FIX set value */}
+                    <button>submit</button>
                 </form>
                 <a>continue as guest</a>
             </div>
@@ -53,20 +116,41 @@ export class AuthText extends React.Component{
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export class MainText extends React.Component{
     render(){
         return(
             <div className="main-text">
                 <div className="main-text__intro">
-                    <p className="main-text__p">Whether you use a straight or double edge razor, or just have sensitive skin and want a better shave, knowing the direction of your beard growth is important. Shave direction effects the closeness of your shave, any skin irritation you experience, and even ingrown hairs & cuts. Use this app to map your beard.</p>
-                    <a className="main-text__a">Continue as guest</a>
-                    <p className="main-text__p"> or </p>
-                    <a className="main-text__a">create account/log in</a>
+                    <p className="main-text__p">Whether you use a straight or double edge razor, or just have sensitive skin and want a better shave, knowing the direction of your beard growth is important.</p>
+                    <p className="main-text__p">Shave direction effects the closeness of your shave, any skin irritation you experience, and even ingrown hairs & cuts. Use this app to map your beard.</p>
+                    {/* <a className="main-text__a main-text__a-guest">continue as guest&nbsp;</a>
+                    <p className="main-text__link-break">or</p>
+                    <a className="main-text__a main-text__a-auth">&nbsp;create account/log in</a> */}
                 </div>
                 <div className="main-text__menu">
-                    <a className="main-text__a">How to determine the direction your hair grows in</a>
-                    <a className="main-text__a main-text__a_strong">Begin mapping</a>
-                    <a className="main-text__a">More information</a>
+                    <a className="main-text__a main-text__menu-item">how to determine growth direction</a>
+                    <a className="main-text__a main-text__a_strong main-text__menu-item">begin mapping</a>
+                    <a className="main-text__a main-text__menu-item">more information</a>
                 </div>
             </div>
         )
